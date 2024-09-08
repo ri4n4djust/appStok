9aa859e2f6ae51f3bf54f73d939ad8514fc66cd3
ri4nb4gus <rian.adjust@gmail.com>
2024-09-09 07:59:11 +0800

    update

--- resources/js/src/views/coa/biayaPenjualan.vue
+++ resources/js/src/views/coa/biayaPenjualan.vue
@@ -40,7 +40,9 @@
                                                 <tbody  v-for="hrt, index in hartalist" :key="hrt.acc_id" :set="amount = hrt.amount">
                                                     
                                                         <tr v-if="hrt.level === '1'" >
-                                                            <td v-if="hrt.jenis != 'Total'" style="min-width:70px">{{ hrt.acc_id }}</td>
+                                                            <td v-if="hrt.jenis != 'Total'" style="min-width:70px">{{ hrt.acc_id }}
+                                                                <a href="javascript:void(0);" title="Edit" @click="edit_acc(accid=hrt.acc_id, level=hrt.level, parent=hrt.parent, name=hrt.name, jenis=hrt.jenis)">edit</a>
+                                                            </td>
                                                             <td v-else></td>
                                                             <td v-if="hrt.jenis === 'Total' || hrt.jenis.substring(0,1) === 'H'" style="min-width: 400px;" ><b>&nbsp;&nbsp;{{ hrt.name }}</b></td>
                                                             <td v-else style="min-width: 400px;">&nbsp;&nbsp;{{ hrt.name }}</td>
@@ -59,7 +61,9 @@
                                                             <td v-else></td>
                                                         </tr>
                                                         <tr v-if="hrt.level === '2'">
-                                                            <td v-if="hrt.jenis != 'Total'" >{{ hrt.acc_id }}</td>
+                                                            <td v-if="hrt.jenis != 'Total'" >{{ hrt.acc_id }}
+                                                                <a href="javascript:void(0);" title="Edit" @click="edit_acc(accid=hrt.acc_id, level=hrt.level, parent=hrt.parent, name=hrt.name, jenis=hrt.jenis)">edit</a>
+                                                            </td>
                                                             <td v-else></td>
                                                             <td v-if="hrt.jenis === 'Total' || hrt.jenis.substring(0,1) === 'H'" style="min-width: 300px;"><b>&nbsp;&nbsp;&nbsp;&nbsp;{{ hrt.name }}</b></td>
                                                             <td v-else style="min-width: 300px;">&nbsp;&nbsp;&nbsp;&nbsp;{{ hrt.name }}</td>
@@ -72,7 +76,9 @@
                                                             <td></td>
                                                         </tr>
                                                         <tr v-if="hrt.level === '3'">
-                                                            <td v-if="hrt.jenis != 'Total'">{{ hrt.acc_id }}</td>
+                                                            <td v-if="hrt.jenis != 'Total'">{{ hrt.acc_id }}
+                                                                <a href="javascript:void(0);" title="Edit" @click="edit_acc(accid=hrt.acc_id, level=hrt.level, parent=hrt.parent, name=hrt.name, jenis=hrt.jenis)">edit</a>
+                                                            </td>
                                                             <td v-else></td>
                                                             <td v-if="hrt.jenis === 'Total' || hrt.jenis.substring(0,1) === 'H'" style="min-width: 300px;"><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ hrt.name }}</b></td>
                                                             <td v-else style="min-width: 300px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ hrt.name }}</td>
@@ -84,7 +90,9 @@
                                                             <td></td>
                                                         </tr>
                                                         <tr v-if="hrt.level === '4'">
-                                                            <td>{{ hrt.acc_id }}</td>
+                                                            <td>{{ hrt.acc_id }}
+                                                                <a href="javascript:void(0);" title="Edit" @click="edit_acc(accid=hrt.acc_id, level=hrt.level, parent=hrt.parent, name=hrt.name, jenis=hrt.jenis)">edit</a>
+                                                            </td>
                                                             <td style="min-width: 300px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ hrt.name }}</td>
                                                             <td>{{ Number(amount).toLocaleString() }}</td>
                                                             <td></td>
@@ -101,6 +109,37 @@
                                 </div>
                             </div>
                         </div>
+                        <Modal v-model:visible="isVisible" :draggable="true" :title="edit">
+                            <input type="text" class="form-control" v-model="selected.oldid" />
+                            <div class="input-group input-group-sm mb-4">
+                                <select v-model="selected.parent" class="form-control">
+                                    <option v-for="hrt in hartalist" :key="hrt.acc_id" :value="hrt.parent">Acc Parent {{ hrt.parent }}</option>
+                                </select>
+                            </div>
+                            <div class="input-group input-group-sm mb-4">
+                                <select v-model="selected.level" class="form-control">
+                                    <option v-for="hrt in hartalist" :key="hrt.acc_id" :value="hrt.level">Level {{ hrt.level }}</option>
+                                </select>
+                            </div>
+                            {{ selected.jenis }}
+                            <div class="input-group mb-4">
+                                <select v-model="selected.jenis" class="form-control" >
+                                    <option v-for="hrt in hartalist" :key="hrt.acc_id" :value="hrt.jenis">{{ hrt.jenis }}</option>
+                                </select>
+                                
+                            </div>
+                            <div class="input-group input-group-sm mb-4">
+                                <select v-model="selected.accid" class="form-control" disabled>
+                                    <option v-for="hrt in hartalist" :key="hrt.acc_id" :value="hrt.acc_id">{{ hrt.name }}</option>
+                                </select>
+                            </div>
+                            <div class="input-group input-group-sm mb-4">
+                                <input type="text" class="form-control" v-model="selected.name" />
+                            </div>
+                            <div class="input-group input-group-sm mb-4">
+                                <a href="javascript:;" @click="simpan_acc()" class="btn btn-success btn-download">Simpan</a>
+                            </div>
+                        </Modal>
                         <div class="col-xl-3">
                             <div class="invoice-actions-btn">
                                 <div class="invoice-action-btn">
@@ -139,6 +178,7 @@
     import { useStore } from 'vuex';
     import { useRouter, useRoute } from 'vue-router';
 
+    import { Modal } from 'usemodal-vue3';
     import moment from "moment";
 
     import { useMeta } from '@/composables/use-meta';
@@ -149,6 +189,16 @@
     const router = useRouter();
     const route = useRoute();
 
+    const isVisible = ref(false);
+    const selected = ref({
+        accid: '',
+        name: '',
+        level: '',
+        parent: '',
+        jenis: '',
+        oldid: '',
+        amount: 0
+    });
 
     // const modalRef = ref(null);
     // const openModal = () => Modal.getInstance(modalRef.value)?.show();
@@ -165,5 +215,33 @@
         }, 3000);
        
     })
+
+    const edit_acc = (accid, level, parent, name, jenis) =>{
+        // alert('yg di edit : '+ accid + ' level : '+ level + 'parent : '+ parent);
+        selected.value.accid = accid ;
+        selected.value.level = level ;
+        selected.value.parent = parent ;
+        selected.value.name = name ;
+        selected.value.jenis = jenis ;
+        isVisible.value = true;
+        // console.log(isVisible.value)
+
+    }
+
+    const simpan_acc = () => {
+        store.dispatch('CreateAcc', selected.value).then((res) => {
+            if (res.data.success === true) {
+                // console.log('berhasil');
+                store.dispatch('GetHarta', { group: '5' });
+                setTimeout(function() { 
+                    // store.dispatch('GetCoaList')
+                    hartalist.value = store.getters.StateHarta;
+                }, 3000);
+                isVisible.value = false;
+            } else {
+                console.log('gagal');
+            }
+        });
+    }
     
 </script>
